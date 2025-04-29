13edae6acf425c1a6849e116e3ab64d171db5b52
shin-yamashita1182 <tsc.yamashita@gmail.com>
2025-04-29 15:39:34 +0900

    Create map_controller.js

---
+++ public/map_controller.js
@@ -0,0 +1,19 @@
+function updateGoogleMap(regionName) {
+  const mapContainer = document.getElementById('map'); // 地図エリアのdiv
+  const encodedRegion = encodeURIComponent(regionName);
+
+  const apiKey = 'AIzaSyA-DU92xdbCPQLQsjQnBiMm-Td3fht-0N0'; // ★本番キーをここに！
+
+  const mapEmbedUrl = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${encodedRegion}`;
+
+  mapContainer.innerHTML = `
+    <iframe
+      width="100%"
+      height="400px"
+      frameborder="0"
+      style="border:0; width:100%; height:100%;"
+      src="${mapEmbedUrl}"
+      allowfullscreen>
+    </iframe>
+  `;
+}
